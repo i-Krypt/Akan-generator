89ec060f0ec21e2d380dc323d98a55a198e92113
i-Krypt <vamay505@gmail.com>
2019-07-22 13:02:38 +0300

    updates

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+// validate
+function verify(){
+  var date = document.getElementById('Date').value;
+  var month = document.getElementById('Month').value;
+  var yearOfBirth = document.getElementById('Year').value;
+
+  if(date===""||date<1||date>31){
+    alert("Date must be between 1 and 31");
+  }
+  else if(month===""||month<1||month>12){
+    alert("Date must be between 1 and 12");
+  }
+  else if(yearOfBirth===""){
+    alert("You must enter a value year");
+  }
+  else{
+    return false;
+  }
+}
+
+
+
+
 function getName() {
   var date = document.getElementById('Date').value;
 
@@ -16,8 +39,8 @@ function getName() {
   var femaleName = ["Akosu", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Amna"];
   var dayOfWeek = ["Sunday", "Monday", "Tuesday", "Wensday", "Thursday", "Friday", "Sunday"];
 
-  var dayOfBirth = (((century / 4) - 2 * year - 1) + ((5 * year / 4)) + ((26 * (month + 1) / 10)) + date) % 7;
-  var dayOfBirth = Math.floor(dayOfBirth);
+  var dayOfBirth = parseInt((((century / 4) - 2 * year - 1) + ((5 * year / 4)) + ((26 * (month + 1) / 10)) + date) % 7);
+  // var dayOfBirth = Math.floor(dayOfBirth);
 
   if (gender === "Male") {
     document.getElementById("result").innerHTML = "You were born on " + dayOfWeek[dayOfBirth - 1] + " and your name is " + maleName[dayOfBirth - 1];
